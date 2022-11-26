0ce04a4a03fa3b7ef0dae25dcc4e975b3fa7d8c5
Tamado Sitohang <ramot@ramottamado.dev>
2022-11-26 23:44:51 +0700

    Add codes to authenticate current user for XWayland because Mutter always breaking things

--- src/spawn-xwayland.c
+++ src/spawn-xwayland.c
@@ -1,3 +1,5 @@
+#include <string.h>
+#include <glib-2.0/glib.h>
 #include <X11/Xlib.h>
 
 #define NIL (0)
@@ -5,6 +7,19 @@
 int main()
 {
     Display *dpy = XOpenDisplay(NIL);
+    XHostAddress host_entry;
+    XServerInterpretedAddress siaddr;
+
+    siaddr.type = (char *)"localuser";
+    siaddr.typelength = strlen(siaddr.type);
+    siaddr.value = (char *)g_get_user_name();
+    siaddr.valuelength = strlen(siaddr.value);
+
+    host_entry.family = FamilyServerInterpreted;
+    host_entry.address = (char *)&siaddr;
+
+    XAddHost(dpy, &host_entry);
     XCloseDisplay(dpy);
+
     return 0;
 }
